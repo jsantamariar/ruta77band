94b2bada7e97ef995a1f3feb6e23eaa729881ca2
“jsantamariar” <santamaria.web.dev@gmail.com>
2025-01-10 22:53:26 -0500

    fix: changing cover picture

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       </div>
       <main className="relative h-full w-full">
         <Image
-          src="/ruta77band.webp"
-          className="object-cover h-full w-full opacity-80"
+          src="/complete-band.jpg"
+          className="object-cover opacity-80"
           alt="Ruta 77 background"
           layout="fill"
         />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -54,13 +55,13 @@ export const Navbar = () => {
         >
           <ul className="font-medium flex flex-col ml-5 p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             <li>
-              <a
-                href="#"
+              <Link
+                href="/"
                 className="block py-2 px-3 text-white rounded md:bg-transparent md:p-0"
                 aria-current="page"
               >
                 Ruta 77
-              </a>
+              </Link>
             </li>
             <li>
               <a
