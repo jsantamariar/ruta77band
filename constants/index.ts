bdba15782868ecc4b3745419a512fd25daa38616
“jsantamariar” <santamaria.web.dev@gmail.com>
2025-01-10 22:26:38 -0500

    fix: fixing ws msg

--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,6 @@ export const SOCIAL_MEDIA_URLS = {
   instagram: "https://www.instagram.com/ruta77band/",
   tiktok: "https://www.tiktok.com/@ruta77band",
   whatsapp:
-    "https://wa.me/+573147471243?text=Hola%20Ruta77%20👋.%20Quiero%20saber%20más%20sobre%20las%presentaciones",
+    "https://wa.me/+573147471243?text=Hola%20Ruta77.%20Quiero%20saber%20más%20sobre%20las%20presentaciones",
   youtube: "https://www.youtube.com/@ruta77band",
 };
